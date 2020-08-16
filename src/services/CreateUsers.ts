42847b79c10ea8970a91ee62be102c2130cd882c
=Bruno Igor <=brigor7@gmail.com>
2020-08-16 15:05:55 -0300

    Usando interfaces para definir atributos e tipos

--- src/services/CreateUsers.ts
+++ src/services/CreateUsers.ts
@@ -1,8 +1,10 @@
-export default function CreateUsers(
-  name = '',
-  email: string,
-  password: string
-) {
+interface CreateUserData {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+export default function CreateUsers({ name, email, password }: CreateUserData) {
   const user = {
     name,
     email,
